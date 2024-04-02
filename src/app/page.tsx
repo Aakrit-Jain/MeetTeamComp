1e078bea9992d75402eb77b3f884fcb477fadec8
Aakrit-Jain <161477421+Aakrit-Jain@users.noreply.github.com>
2024-04-02 15:53:07 +0530

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import Instructors from "@/components/Tooltip";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Instructors/>
+    </main>
+  );
+}
---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,156 @@
+'use client'
+import React from 'react'
+import { BackgroundGradient } from "./ui/background-gradient";
+import Image from "next/image";
+
+
+function Instructors() {
+  return (
+    <div className='h-auto w-auto dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] overflow-hidden'>
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <div>
+            <div className='text-center'>
+                <h2 className='text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8'>Meet the Team</h2>
+            </div>
+            <div className='mt-10'>
+                <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-12 px-10 justify-center'>
+                <BackgroundGradient className="rounded-[22px]  p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+        <BackgroundGradient className="rounded-[22px] p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80`}
+          alt="jordans"
+          height="200"
+          width="200"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Lorem Ipsum
+        </p>
+        </BackgroundGradient>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Instructors;
